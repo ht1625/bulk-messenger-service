248a3b06163f5d4cc79d32467e3d1f2a868ae1e6
ht1625 <61890580+ht1625@users.noreply.github.com>
2026-01-20 02:27:31 +0300

    fix(ci): use Cache::fake() in SendMessageJob test

--- tests/Feature/Jobs/SendMessageJobTest.php
+++ tests/Feature/Jobs/SendMessageJobTest.php
@@ -15,6 +15,8 @@ class SendMessageJobTest extends TestCase
 
     public function test_job_marks_message_as_sent_after_successful_send(): void
     {
+        Cache::fake();
+
         Http::fake([
             '*' => Http::response([
                 'messageId' => 'abc-123'
